6b4b213288d88bf7334d4952d5c7b0c00df013a8
Lucas Santos <lucas95santos@gmail.com>
2021-07-24 15:19:20 -0400

    chore: configurando ambiente de desenvolvimento

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
+    'import/prefer-default-export': 'off',
+    'jsx-quotes': ['error', 'prefer-single'],
+  },
+};
---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  allowParens: 'avoid',
+  tabWidth: 2,
+  semi: true,
+};
