9d0ef6eccb069c3ba9b56429568722ce31ca6b21
Lucas Santos <lucas95santos@gmail.com>
2021-07-24 21:39:49 -0400

    chore: aplicando novas regras

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,7 @@ module.exports = {
     'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
     'import/prefer-default-export': 'off',
     'jsx-quotes': ['error', 'prefer-double'],
+    'react/prop-types': 'off',
+    'react/jsx-props-no-spreading': 'off',
   },
 };
