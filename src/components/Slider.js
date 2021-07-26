aececdb026f35d86ead7fae2ef5814b43b2192d0
Lucas Santos <lucasa95santos@gmail.com>
2021-07-25 23:26:37 -0400

    refactor: adaptando o Slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,7 +1,7 @@
-/* eslint-disable global-require */
-/* eslint-disable import/no-dynamic-require */
 import React from 'react';
 import SliderLib from 'react-slick';
+// icons
+import { HiOutlineChevronLeft, HiOutlineChevronRight } from 'react-icons/hi';
 // styles
 import '../styles/slider.scss';
 
@@ -12,22 +12,51 @@ const settings = {
   speed: 500,
   slidesToShow: 1,
   slidesToScroll: 1,
+  draggable: true,
 };
 
-const Slider = ({ type = 'image', images }) => (
-  <SliderLib {...settings} className="slider">
-    {type === 'image' ? (
-      images.map((image, index) => (
-        <div className="slider__image" key={image.id}>
-          <img src={image.url} alt={`Imagem ${index + 1}`} />
-        </div>
-      ))
-    ) : (
-      <div>
-        <h1>1</h1>
-      </div>
-    )}
-  </SliderLib>
+const PrevArrow = ({ className, style, onClick }) => (
+  <HiOutlineChevronLeft
+    className={`${className} prev-arrow`}
+    style={{ ...style, fontSize: '32px !important' }}
+    onClick={onClick}
+  />
 );
+
+const NextArrow = ({ className, style, onClick }) => (
+  <HiOutlineChevronRight
+    className={`${className} prev-arrow`}
+    style={{ ...style, fontSize: '32px !important' }}
+    onClick={onClick}
+  />
+);
+
+const Slider = ({ type = 'image', images, children, rest }) => {
+  settings.arrows = type !== 'image';
+  settings.dots = type === 'image';
+  settings.slidesToShow = type === 'image' ? 1 : 2;
+  settings.slidesToScroll = type === 'image' ? 1 : 2;
+  settings.draggable = type === 'image';
+  settings.prevArrow = <PrevArrow />;
+  settings.nextArrow = <NextArrow />;
+
+  return (
+    <>
+      {type === 'image' ? (
+        <SliderLib {...settings} className="slider" {...rest}>
+          {images.map((image, index) => (
+            <div className="slider__image" key={image.id}>
+              <img src={image.url} alt={`Imagem ${index + 1}`} />
+            </div>
+          ))}
+        </SliderLib>
+      ) : (
+        <SliderLib {...settings} className="slider" {...rest}>
+          {children}
+        </SliderLib>
+      )}
+    </>
+  );
+};
 
 export { Slider };
