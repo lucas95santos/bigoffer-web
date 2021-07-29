4a09c7c7f8fc103052786d16486f466e011f57d7
Lucas Santos <lucasa95santos@gmail.com>
2021-07-28 22:47:58 -0400

    feat: implementando o contexto global na raiz do projeto

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 // routes
 import Routes from './routes';
+// global context provider
+import GlobalProvider from './contexts/global';
 
 const App = () => (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <GlobalProvider>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </GlobalProvider>
 );
 
 export default App;
