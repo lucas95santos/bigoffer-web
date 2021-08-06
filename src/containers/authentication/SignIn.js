b2a8a80ae5b0553820093701714e80793512b5a6
Lucas Santos <lucasa95santos@gmail.com>
2021-08-06 00:13:44 -0400

    feat: implementando estado nos componentes de autenticação

--- src/containers/authentication/SignIn.js
+++ src/containers/authentication/SignIn.js
@@ -1,42 +1,67 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 
-const SignIn = ({
-  onAuthenticationSubmit,
-  changeAuthenticationTo,
-  authentication,
-}) => (
-  <div className="authentication">
-    <div className="authentication__title">
-      <h2>
-        Informe seu email <br /> e senha nos campos <br /> abaixo
-      </h2>
-    </div>
+const SignIn = React.memo(
+  ({ onAuthenticationSubmit, changeAuthenticationTo, authentication }) => {
+    const [signInData, setSignInData] = useState({
+      email: '',
+      password: '',
+    });
 
-    <form onSubmit={onAuthenticationSubmit} className="authentication__form">
-      <input type="email" required placeholder="Digite seu email aqui" />
-      <input
-        type="password"
-        required
-        placeholder="Digite sua senha aqui"
-        className="mt_16"
-      />
+    const handleInputChange = useCallback((field, value) => {
+      setSignInData((oldSignInData) => ({
+        ...oldSignInData,
+        [field]: value,
+      }));
+    }, []);
 
-      <button type="submit" className="solid">
-        Acessar
-      </button>
-    </form>
+    return (
+      <div className="authentication">
+        <div className="authentication__title">
+          <h2>
+            Informe seu email <br /> e senha nos campos <br /> abaixo
+          </h2>
+        </div>
 
-    <div className="authentication__footer">
-      <small>Não possui conta?</small>
-      <button
-        type="button"
-        className="outlined no-border"
-        onClick={() => changeAuthenticationTo(authentication.SIGN_UP)}
-      >
-        Cadastre-se agora
-      </button>
-    </div>
-  </div>
+        <form
+          onSubmit={onAuthenticationSubmit}
+          className="authentication__form"
+        >
+          <input
+            type="email"
+            required
+            placeholder="Digite seu email aqui"
+            value={signInData.email}
+            onChange={(event) => handleInputChange('email', event.target.value)}
+          />
+          <input
+            type="password"
+            required
+            placeholder="Digite sua senha aqui"
+            className="mt_16"
+            value={signInData.password}
+            onChange={(event) =>
+              handleInputChange('password', event.target.value)
+            }
+          />
+
+          <button type="submit" className="solid">
+            Acessar
+          </button>
+        </form>
+
+        <div className="authentication__footer">
+          <small>Não possui conta?</small>
+          <button
+            type="button"
+            className="outlined no-border"
+            onClick={() => changeAuthenticationTo(authentication.SIGN_UP)}
+          >
+            Cadastre-se agora
+          </button>
+        </div>
+      </div>
+    );
+  },
 );
 
 export default SignIn;
--- src/containers/authentication/SignUp.js
+++ src/containers/authentication/SignUp.js
@@ -1,46 +1,93 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 
-const SignUp = ({
-  onAuthenticationSubmit,
-  changeAuthenticationTo,
-  authentication,
-}) => (
-  <div className="authentication">
-    <div className="authentication__title">
-      <h2>
-        Preencha os campos abaixo
-        <br /> com os seus dados
-        <br /> para criar sua conta
-      </h2>
-    </div>
+const SignUp = React.memo(
+  ({ onAuthenticationSubmit, changeAuthenticationTo, authentication }) => {
+    const [signUpData, setSignUpData] = useState({
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    });
 
-    <form onSubmit={onAuthenticationSubmit} className="authentication__form">
-      <div className="input-inline">
-        <input type="text" required placeholder="Digite seu nome" />
-        <input type="email" required placeholder="Digite sua email aqui" />
+    const handleInputChange = useCallback((field, value) => {
+      setSignUpData((oldSignUpData) => ({
+        ...oldSignUpData,
+        [field]: value,
+      }));
+    }, []);
+
+    return (
+      <div className="authentication">
+        <div className="authentication__title">
+          <h2>
+            Preencha os campos abaixo
+            <br /> com os seus dados
+            <br /> para criar sua conta
+          </h2>
+        </div>
+
+        <form
+          onSubmit={onAuthenticationSubmit}
+          className="authentication__form"
+        >
+          <div className="input-inline">
+            <input
+              type="text"
+              required
+              placeholder="Digite seu nome"
+              value={signUpData.name}
+              onChange={(event) =>
+                handleInputChange('name', event.target.value)
+              }
+            />
+            <input
+              type="email"
+              required
+              placeholder="Digite sua email aqui"
+              value={signUpData.email}
+              onChange={(event) =>
+                handleInputChange('email', event.target.value)
+              }
+            />
+          </div>
+
+          <div className="input-inline">
+            <input
+              type="password"
+              placeholder="Digite uma senha"
+              value={signUpData.password}
+              onChange={(event) =>
+                handleInputChange('password', event.target.value)
+              }
+            />
+            <input
+              type="password"
+              placeholder="Confirme sua senha"
+              value={signUpData.confirmPassword}
+              onChange={(event) =>
+                handleInputChange('confirmPassword', event.target.value)
+              }
+            />
+          </div>
+
+          <button type="submit" className="solid">
+            Criar conta
+          </button>
+        </form>
+
+        <div className="authentication__footer">
+          <small>Já possui conta?</small>
+          <button
+            type="button"
+            className="outlined no-border"
+            onClick={() => changeAuthenticationTo(authentication.SIGN_IN)}
+          >
+            Faça seu login
+          </button>
+        </div>
       </div>
-
-      <div className="input-inline">
-        <input type="password" placeholder="Digite uma senha" />
-        <input type="password" placeholder="Confirme sua senha" />
-      </div>
-
-      <button type="submit" className="solid">
-        Criar conta
-      </button>
-    </form>
-
-    <div className="authentication__footer">
-      <small>Já possui conta?</small>
-      <button
-        type="button"
-        className="outlined no-border"
-        onClick={() => changeAuthenticationTo(authentication.SIGN_IN)}
-      >
-        Faça seu login
-      </button>
-    </div>
-  </div>
+    );
+  },
 );
 
 export default SignUp;
