e7c705838d252f384c9c48bddfc2176e2571173b
Lucas Santos <lucasa95santos@gmail.com>
2021-08-22 21:09:41 -0400

    feat: finalizando a estrutura das configurações de pagamento

--- src/screens/settings/Payment.js
+++ src/screens/settings/Payment.js
@@ -4,6 +4,17 @@ import React, { useState, useCallback } from 'react';
 import CreditCard from 'react-credit-cards';
 // components
 import { CustomInput as Input } from '../../components';
+// icons
+import { AiOutlineFieldNumber, AiOutlineIdcard } from 'react-icons/ai';
+import { MdDateRange } from 'react-icons/md';
+import { FiLock } from 'react-icons/fi';
+// assets
+import fakeCreditCardImg from '../../assets/credit-cards/master.png';
+
+const payment = {
+  LIST_OPTIONS: 0,
+  ADD_OPTION: 1,
+};
 
 const Payment = ({ settingsTitle }) => {
   // eslint-disable-next-line no-unused-vars
@@ -14,6 +25,7 @@ const Payment = ({ settingsTitle }) => {
     number: '',
   });
   const [focus, setFocus] = useState('');
+  const [currentContent, setCurrentContent] = useState(payment.LIST_OPTIONS);
 
   const handleInputChange = useCallback((field, value) => {
     setCreditCardData((oldCreditCardData) => ({
@@ -31,94 +43,133 @@ const Payment = ({ settingsTitle }) => {
     <div>
       <h2>{settingsTitle}</h2>
 
-      <div className="payment-container">
-        <form onSubmit={onSavePaymentOption}>
-          <fieldset>
-            <div className="input-container">
-              <Input
-                type="text"
-                name="number"
-                placeholder="Número do cartão"
-                value={creditCardData.number}
-                onTextChange={(event) =>
-                  handleInputChange('number', event.target.value)
-                }
-                onFocus={setFocus}
-                maxLength={16}
-                errors={[]}
-                autocomplete="nope"
-              />
-            </div>
+      {currentContent === payment.ADD_OPTION ? (
+        <div className="add-payment">
+          <form onSubmit={onSavePaymentOption}>
+            <fieldset>
+              <div className="input-container">
+                <Input
+                  type="text"
+                  name="number"
+                  placeholder="Número do cartão"
+                  value={creditCardData.number}
+                  onTextChange={(event) =>
+                    handleInputChange('number', event.target.value)
+                  }
+                  onFocus={setFocus}
+                  maxLength={16}
+                  icon={AiOutlineFieldNumber}
+                  errors={[]}
+                  autocomplete="nope"
+                />
+              </div>
 
-            <div className="input-container">
-              <Input
-                type="text"
-                name="name"
-                placeholder="Nome no cartão"
-                value={creditCardData.name}
-                onTextChange={(event) =>
-                  handleInputChange('name', event.target.value)
-                }
-                onFocus={setFocus}
-                errors={[]}
-                autocomplete="nope"
-              />
-            </div>
+              <div className="input-container">
+                <Input
+                  type="text"
+                  name="name"
+                  placeholder="Nome no cartão"
+                  value={creditCardData.name}
+                  onTextChange={(event) =>
+                    handleInputChange('name', event.target.value)
+                  }
+                  onFocus={setFocus}
+                  icon={AiOutlineIdcard}
+                  errors={[]}
+                  autocomplete="nope"
+                />
+              </div>
 
-            <div className="input-container">
-              <Input
-                type="text"
-                name="expiry"
-                placeholder="Expiração"
-                classes="half-size"
-                value={creditCardData.expiry}
-                onTextChange={(event) =>
-                  handleInputChange('expiry', event.target.value)
-                }
-                onFocus={setFocus}
-                maxLength={4}
-                errors={[]}
-                autocomplete="nope"
-              />
+              <div className="input-container">
+                <Input
+                  type="text"
+                  name="expiry"
+                  placeholder="Expiração"
+                  classes="half-size"
+                  value={creditCardData.expiry}
+                  onTextChange={(event) =>
+                    handleInputChange('expiry', event.target.value)
+                  }
+                  onFocus={setFocus}
+                  maxLength={4}
+                  icon={MdDateRange}
+                  errors={[]}
+                  autocomplete="nope"
+                />
 
-              <Input
-                type="text"
-                name="cvc"
-                placeholder="Código de segurança"
-                classes="half-size"
-                value={creditCardData.cvc}
-                onTextChange={(event) =>
-                  handleInputChange('cvc', event.target.value)
-                }
-                onFocus={setFocus}
-                maxLength={3}
-                errors={[]}
-                autocomplete="nope"
-              />
-            </div>
-          </fieldset>
+                <Input
+                  type="text"
+                  name="cvc"
+                  placeholder="CVV"
+                  classes="half-size"
+                  value={creditCardData.cvc}
+                  onTextChange={(event) =>
+                    handleInputChange('cvc', event.target.value)
+                  }
+                  onFocus={setFocus}
+                  maxLength={3}
+                  icon={FiLock}
+                  errors={[]}
+                  autocomplete="nope"
+                />
+              </div>
+            </fieldset>
 
-          <button className="solid" type="submit">
-            Salvar opção
-          </button>
-        </form>
+            <footer className="add-payment-footer">
+              <button className="solid" type="submit">
+                Salvar opção
+              </button>
+              <button
+                type="button"
+                className="outlined cancel"
+                onClick={() => setCurrentContent(payment.LIST_OPTIONS)}
+              >
+                Cancelar
+              </button>
+            </footer>
+          </form>
 
-        <div>
-          <CreditCard
-            cvc={creditCardData.cvc}
-            expiry={creditCardData.expiry}
-            name={creditCardData.name}
-            number={creditCardData.number}
-            focused={focus}
-            placeholders={{
-              name: 'Seu nome aqui',
-            }}
-            locale={{
-              valid: 'válido até',
-            }}
-          />
+          <div className="credit-card">
+            <CreditCard
+              cvc={creditCardData.cvc}
+              expiry={creditCardData.expiry}
+              name={creditCardData.name}
+              number={creditCardData.number}
+              focused={focus}
+              placeholders={{
+                name: 'Seu nome aqui',
+              }}
+              locale={{
+                valid: 'válido até',
+              }}
+            />
+          </div>
         </div>
-      </div>
+      ) : (
+        <ul className="payment-options">
+          <li>
+            <div className="option">
+              <img src={fakeCreditCardImg} alt="" className="option__image" />
+              <p className="option__number">
+                <strong>**** **** ****</strong> 8302
+              </p>
+              <small>Expira em 02/30</small>
+            </div>
+            <button type="button" className="outlined no-border">
+              Remover
+            </button>
+          </li>
+          <li className="new-option">
+            <button
+              type="button"
+              className="outlined"
+              onClick={() => setCurrentContent(payment.ADD_OPTION)}
+            >
+              Adicionar nova opção
+            </button>
+          </li>
+        </ul>
+      )}
     </div>
   );
 };
