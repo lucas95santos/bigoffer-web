ff140bf0a8ce8711978c8d01be17a42bbd6bf658
Lucas Santos <lucasa95santos@gmail.com>
2021-09-26 23:05:59 -0400

    feat: criando função para ver as notificações

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+// context
+import { GlobalContext } from '../contexts/global';
 // icons
 import { RiCloseLine } from 'react-icons/ri';
 
 const Toast = ({ visible, onClose }) => {
+  // context
+  const { changeAppState } = useContext(GlobalContext);
+  // route
+  const history = useHistory();
+
+  // handlers
+  const goToNotifications = () => {
+    onClose();
+    changeAppState('global', 'LOADING');
+    history.push('/notificacoes');
+  };
+
   if (!visible) return null;
 
   return (
@@ -14,6 +29,7 @@ const Toast = ({ visible, onClose }) => {
             className="outlined no-border"
             type="button"
             title="Ver a notificação agora"
+            onClick={() => goToNotifications()}
           >
             Ver agora
           </button>
